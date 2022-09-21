12a722bbba4e0916426f81e26537467e7ec481a1
Лидия Дворкина <lideya832@mail.ru>
2022-09-21 21:56:20 +0300

    Ошибки испрвила. Ничего красным не выделено, но в консоль не выводит ни одной строки.

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,45 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){}
-                public static int[] generateRandomArray() {
+    public static void main(String[] args) {
+    }
+
+    public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
             arr[i] = random.nextInt(100_000) + 100_000;
-            int [] expenses =generateRandomArray();
-            System.out.println(Arrays.toString(expenses));
-            System.out.println("Задание № 1");
-        int totalSum=0;
-            for (int i = 0; i < expenses.length; i++){
-            int expense = expenses[i];
-            totalSum += expense;
-            System.out.println("Сумма трат за месяц составила " +totalSum + "рублей");
-            System.out.println("Задание № 2");
-            int max = Integer. MIN_VALUE;
-            int min = Integer.MAX_VALUE;
-            System.out.println("Минимальная сумма трат за день составила " +min+ "рублей");
-            System.out.println("Максимальная сумма трат за день составила " +max+ "рублей");
-            System.out.println("Задание № 3");
-            double midleExpense = (double) totalSum/expenses.length;
-            System.out.println("Средняя сумма трат за месяц составила " +midleExpense+ " рублей");
-            System.out.println("Задание № 4");
-            char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-            for (int i = reverseFullName.length - 1; i>=0; i--);
-            System.out.println(reverseFullName[i]);
+        int[] expenses = generateRandomArray();
+        System.out.println(Arrays.toString(expenses));
 
-        }
+                    System.out.println("Задание № 1");
+                    int totalSum = 0;
+                    for (int i = 0; i < expenses.length; i++) {
+                        int expense = expenses[i];
+                        totalSum += expense;}
+                        System.out.println("Сумма трат за месяц составила " + totalSum + "рублей");
+                        System.out.println("Задание № 2");
+                        int max = Integer.MIN_VALUE;
+                        int min = Integer.MAX_VALUE;
+                        System.out.println("Минимальная сумма трат за день составила " + min + "рублей");
+                        System.out.println("Максимальная сумма трат за день составила " + max + "рублей");
+                        System.out.println("Задание № 3");
+                        double midleExpense = (double) totalSum / expenses.length;
+                        System.out.println("Средняя сумма трат за месяц составила " + midleExpense + " рублей");
+                        System.out.println("Задание № 4");
+                        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+                        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                            System.out.println(reverseFullName[i]);
+                        }
+                        return arr;
+                        }}
 
-        }
-            return arr;}}
+
+
+
+
+
+
 
 
 
